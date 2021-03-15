81c6e959fc3e1a8cb3e75d5c07f8a2cc7ba5bdcf
Yaritza <yaritzamicaelleb@gmail.com>
2021-03-15 08:49:24 -0300

    Tarefa Medidas

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string entrada;
+            double metros, centimetros, quilometros;
+
+            Console.Beep();
+            Console.WriteLine("---------");
+            Console.Write("Digite a medida (em metros): ");
+            entrada = Console.ReadLine();
+            metros = Convert.ToDouble(entrada);
+            centimetros = metros * 100;
+            quilometros = metros / 100;
+
+            Console.Beep();
+            Console.WriteLine("----------");
+            Console.ForegroundColor=ConsoleColor.DarkRed;
+            Console.WriteLine("\n---Equivalencia---");
+            Console . ResetColor ();
+            Console.WriteLine($"{centimetros:N2} cm");
+            Console.WriteLine($"{metros:N1} m");
+            Console.WriteLine($"{quilometros:N3} km");
+            Console.WriteLine("----------");
+        }
+    }
+}
